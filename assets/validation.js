d3698eee45cc9e796545f21562ca4009159b137a
Gunjan Kumari <gunjankumari230@gmail.com>
2024-06-25 15:52:44 +0530

    add files

---
+++ assets/validation.js
@@ -0,0 +1,83 @@
+﻿function letters(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode == 32 || charCode == 46)
+        return true;
+    else {
+        //alert("Please Enter valid input");
+        return false;
+    }
+}
+function contact(evt) {
+    var cno = document.getElementById("TextBox3").value;
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if ((charCode >= 47 && charCode <= 57)) {
+
+        //alert("Please Enter valid input " + cno.length);
+        if (cno.length <= 9) {
+           // alert("Length " + cno.length);
+            return true;
+        
+        }
+        else
+            return false;
+    }
+    else {
+
+        return false;
+    }
+}
+
+function aadharno(evt) {
+    var cno = document.getElementById("TextBox8").value;
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if ((charCode >= 47 && charCode <= 57)) {
+
+        //alert("Please Enter valid input " + cno.length);
+        if (cno.length <= 11) {
+            // alert("Length " + cno.length);
+            return true;
+
+        }
+        else
+            return false;
+    }
+    else {
+
+        return false;
+    }
+}
+function pincode(evt) {
+    var cno = document.getElementById("TextBox6").value;
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    
+    if ((charCode >= 47 && charCode <= 57)) {
+
+        //alert("Please Enter valid input " + cno.length);
+        if (cno.length <= 5) {
+            // alert("Length " + cno.length);
+            return true;
+
+        }
+        else
+            return false;
+    }
+    else {
+
+        return false;
+    }
+}
+function number(evt) {
+
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if ((charCode >= 47 && charCode <= 57)) {
+
+
+        return true;
+
+
+    }
+    else {
+
+        return false;
+    }
+}
---
+++ login_placement.aspx.vb
@@ -0,0 +1,16 @@
+﻿
+Partial Class login_admin
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "admin" And TextBox2.Text = "admin" Then
+            Response.Redirect("placement_company_display.aspx")
+        Else
+            Response.Write("<script LANGUAGE='JavaScript'>alert('Invalid Username/Password')</script>")
+        End If
+    End Sub
+
+    Protected Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
